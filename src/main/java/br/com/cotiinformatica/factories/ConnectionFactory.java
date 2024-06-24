1b0520ed827e1d17dc5c1ff4e724dc906ac7a6a3
PabloHoties <pablohotiesmarques@gmail.com>
2024-06-24 10:20:38 -0300

    feat: configuradas as classes SwaggerConfig e CorsConfig

--- src/main/java/br/com/cotiinformatica/factories/ConnectionFactory.java
+++ src/main/java/br/com/cotiinformatica/factories/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class ConnectionFactory {
 
 	private static String driver = "org.postgresql.Driver";
-	private static String url = "jdbc:postgresql://localhost:5432/bd_exercicioApi";
+	private static String url = "jdbc:postgresql://localhost:5432/exercicioApi";
 	private static String user = "postgres";
 	private static String pass = "coti";
 
